85974761e302c1d0725ac9cecc1078b92d3cf9d6
RyanAquino <ryan.aquino7110@gmail.com>
2021-06-15 11:51:24 +0800

    ZDL-57: Remove groups.py and add migration of groups

--- conftest.py
+++ conftest.py
@@ -2,11 +2,7 @@ import pytest
 from authentication.tests.factories.user_factory import UserFactory
 from django.test.client import Client
 from pytest_django.lazy_django import skip_if_no_django
-from authentication.tests.factories.group_factory import (
-    CustomersGroupFactory,
-    SuppliersGroupFactory,
-    AdminsGroupFactory,
-)
+from django.contrib.auth.models import Group
 
 
 @pytest.fixture
@@ -18,18 +14,9 @@ def logged_in_client(logged_in_user):
 @pytest.fixture
 def logged_in_user():
     skip_if_no_django()
-
-    user = UserFactory.create(
-        groups=(
-            CustomersGroupFactory.create(),
-            SuppliersGroupFactory.create(),
-            AdminsGroupFactory.create(),
-        )
-    )
-
-    return user
+    return UserFactory.create(groups=Group.objects.all())
 
 
 @pytest.fixture
 def admin_group():
-    UserFactory.create(groups=(AdminsGroupFactory.create(),))
+    UserFactory.create(groups=(Group.objects.filter(name="Admins")))

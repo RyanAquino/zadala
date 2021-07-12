7ec0c4f366ff14f1a9329b772d0e55a44ec7baf1
RyanAquino <ryan.aquino7110@gmail.com>
2021-07-12 09:03:07 +0800

    ZDL-67: Add search filter on Product retrieval

--- orders/tests/test_orders_api.py
+++ orders/tests/test_orders_api.py
@@ -3,6 +3,8 @@ from orders.tests.factories.order_factory import OrderFactory
 from orders.models import OrderItem, Order
 import pytest
 
+from products.tests.factories.product_factory import ProductFactory
+
 
 @pytest.mark.django_db
 def test_list_all_orders_with_empty_database(logged_in_client):
@@ -41,6 +43,43 @@ def test_list_all_orders(logged_in_client, logged_in_user):
     assert order_details["order"] == 1
     assert order_details["quantity"] == 35
     assert order_details["total"] == 1225.0
+
+
+@pytest.mark.django_db
+def test_update_cart_product_id_not_existing(logged_in_client, logged_in_user):
+    """
+    Test update cart with product id not existing
+    """
+    OrderItemFactory()
+    non_existing_product_id = 0
+    data = {"productId": non_existing_product_id, "action": "add"}
+
+    response = logged_in_client.post(f"/v1/orders/update-cart/", data=data)
+    response_data = response.json()
+
+    assert response.status_code == 404
+    assert response_data == {"detail": "Not found."}
+
+
+@pytest.mark.django_db
+def test_update_cart_exceeding_max_product_stock_quantity(
+    logged_in_client, logged_in_user
+):
+    """
+    Test add products to a cart with exceeding product stock quantity
+    """
+    product_quantity = 5
+    order = OrderFactory(customer=logged_in_user)
+    product = ProductFactory(quantity=product_quantity)
+    OrderItemFactory(order=order, product=product, quantity=product_quantity)
+    product_id = product.id
+    data = {"productId": product_id, "action": "add"}
+
+    response = logged_in_client.post(f"/v1/orders/update-cart/", data=data)
+    response_data = response.json()
+
+    assert response.status_code == 422
+    assert response_data == {"detail": "Order Item quantity exceeds available stock"}
 
 
 @pytest.mark.django_db

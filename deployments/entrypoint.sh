251fee17d80fe149085293ab9ef0d7dbb1eed38b
RyanAquino <ryan.aquino7110@gmail.com>
2023-08-01 15:08:56 +0800

    ZDL-122: Convert requirements to poetry pyproject toml

--- deployments/entrypoint.sh
+++ deployments/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+poetry run python manage.py migrate --no-input
+poetry run python manage.py collectstatic --no-input
 
-DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --first_name $SUPER_FIRST_NAME --last_name $SUPER_LAST_NAME --email $SUPER_USER_EMAIL --noinput
+DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD poetry run python manage.py createsuperuser --first_name $SUPER_FIRST_NAME --last_name $SUPER_LAST_NAME --email $SUPER_USER_EMAIL --noinput
 
-gunicorn zadalaAPI.wsgi --bind 0.0.0.0:8000 --workers=$GUNICORN_WORKERS
+poetry run gunicorn zadalaAPI.wsgi --bind 0.0.0.0:8000 --workers=$GUNICORN_WORKERS
